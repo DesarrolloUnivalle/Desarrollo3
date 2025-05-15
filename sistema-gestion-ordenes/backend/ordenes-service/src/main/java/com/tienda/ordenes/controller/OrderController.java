f627cf26afd6a99885e04c2717acbcb40c25a1ed
AlejandroSierraUV26 <sierra.alejandro@correounivalle.edu.co>
2025-05-14 20:49:15 -0500

    SCRUM-65: Actualización vista por correo

--- sistema-gestion-ordenes/backend/ordenes-service/src/main/java/com/tienda/ordenes/controller/OrderController.java
+++ sistema-gestion-ordenes/backend/ordenes-service/src/main/java/com/tienda/ordenes/controller/OrderController.java
@@ -26,6 +26,7 @@ import com.tienda.ordenes.service.EmailService;
 import com.tienda.ordenes.service.OrderService;
 import com.tienda.ordenes.service.impl.OrderServiceImpl;
 import com.tienda.ordenes.model.Order;
+import com.tienda.ordenes.model.OrderStatus;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -63,14 +64,21 @@ public class OrderController {
     }
     
     @PostMapping("/pago-exitoso")
-    public ResponseEntity<String> confirmarPago(@RequestBody ConfirmarPagoRequest request) {
+    public ResponseEntity<OrderResponse> confirmarPago(@RequestBody ConfirmarPagoRequest request) {
         Long orderId = request.getOrderId();
         String emailUsuario = request.getEmailUsuario();
 
-        System.out.println("Confirmando pago para la orden ID: " + orderId + " con el email " + emailUsuario);
+        
 
         Order order = orderRepository.findById(orderId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Orden no encontrada"));
+        if (order.getEstado() != OrderStatus.PENDIENTE) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "La orden #" + orderId + " no puede ser procesada porque su estado actual es: " + order.getEstado().getValue()
+            );
+        }
+        System.out.println("Confirmando pago para la orden ID: " + orderId + " con el email " + emailUsuario);    
 
         UserResponseDTO usuario = usuarioClient.obtenerUsuarioPorEmail(emailUsuario);
 
@@ -78,7 +86,8 @@ public class OrderController {
 
         ((OrderServiceImpl) orderService).procesarPago(order, usuario);
 
+        OrderResponse response = OrderResponse.fromEntity(order);
 
-        return ResponseEntity.ok("Correo de confirmación enviado");
+        return ResponseEntity.ok(response);
     }   
 }
