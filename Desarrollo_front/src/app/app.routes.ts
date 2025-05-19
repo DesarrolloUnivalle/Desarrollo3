c79662eef9f34956e9226cbd2062218710b6d9b9
Esteban080 <estebanrevelo80@gmail.com>
2025-05-19 13:14:50 -0500

    Fix1

--- Desarrollo_front/src/app/app.routes.ts
+++ Desarrollo_front/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
--- Desarrollo_front/src/app/components/login/login.component.ts
+++ Desarrollo_front/src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { UsuarioService } from '../services/usuario.service';
+import { UsuarioService } from '../../services/usuario.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
