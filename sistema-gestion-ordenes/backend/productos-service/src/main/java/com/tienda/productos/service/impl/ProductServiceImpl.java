31d6c672814b14fe9ca01fca82da8fc3d2125854
JuanPosso3 <juan.miguel.posso@correounivalle.edu.co>
2025-05-18 19:27:17 -0500

    Funcionalidad todos los productos

--- sistema-gestion-ordenes/backend/productos-service/src/main/java/com/tienda/productos/service/impl/ProductServiceImpl.java
+++ sistema-gestion-ordenes/backend/productos-service/src/main/java/com/tienda/productos/service/impl/ProductServiceImpl.java
@@ -84,5 +84,10 @@ public class ProductServiceImpl implements ProductService {
         return products;
     }
 
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
     
 }
